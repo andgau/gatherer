84d68f4b2244deca97dfc12665f2fbc328e1fbdc
Andres <andres-gaudioso@outlook.es>
2021-07-24 12:00:28 +0200

    Versión de arranque con logs

---
+++ src/main/java/es/sinjava/recolector/RecolectorApplication.java
@@ -0,0 +1,13 @@
+package es.sinjava.recolector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecolectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecolectorApplication.class, args);
+	}
+
+}
